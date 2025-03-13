3308a578b568365e3a8f5487f260df4b4d656b85
blandk77 <alexags1983@gmail.com>
2025-03-14 00:23:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-TOKEN = '7773651775:AAEH1TN8P5700Ni7fluT9A7uE0xUWuZ0slE'
-MONGO_URI = 'mongodb+srv://telegramguy21:tnkIwvbNkJ5U3fZ7@botsuse.bpgag.mongodb.net/?retryWrites=true&w=majority&appName=Botsuse'
+API_ID = "27394279"
+API_HASH = "90a9aa4c31afa3750da5fd686c410851"
+BOT_TOKEN = "7773651775:AAEH1TN8P5700Ni7fluT9A7uE0xUWuZ0slE"
+MONGO_URL = "mongodb+srv://telegramguy21:tnkIwvbNkJ5U3fZ7@botsuse.bpgag.mongodb.net/?retryWrites=true&w=majority&appName=Botsuse"
