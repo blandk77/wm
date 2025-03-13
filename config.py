7bb30e89b089f12e6cc9debe0c31d7b3d4420333
blandk77 <alexags1983@gmail.com>
2025-03-13 23:39:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = '7825342391:AAEESMCGeZv2HCpYpbhiErXIbcuDrwHOOic'
+TOKEN = '7773651775:AAEH1TN8P5700Ni7fluT9A7uE0xUWuZ0slE'
 MONGO_URI = 'mongodb+srv://telegramguy21:tnkIwvbNkJ5U3fZ7@botsuse.bpgag.mongodb.net/?retryWrites=true&w=majority&appName=Botsuse'
