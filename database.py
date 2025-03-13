07d0b737160b693c1c8a7bd2155c1720cc2de0ab
blandk77 <alexags1983@gmail.com>
2025-03-14 00:20:11 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,14 @@
 import pymongo
+from config import MONGO_URL
 
 class MongoDB:
-    def __init__(self, mongo_uri):
-        self.client = pymongo.MongoClient(mongo_uri)
-        self.db = self.client['watermark_bot']
-        self.collection = self.db['watermark_urls']
+    def __init__(self):
+        self.client = pymongo.MongoClient("mongodb://localhost:27017/")
+        self.db = self.client["watermark_bot"]
+        self.collection = self.db["watermark_urls"]
 
-    def save_watermark_url(self, url, chat_id):
-        self.collection.insert_one({'chat_id': chat_id, 'url': url})
+    def save_watermark_url(self, url, user_id):
+        self.collection.insert_one({"user_id": user_id, "url": url})
 
-    def get_watermark_url(self, chat_id):
-        return self.collection.find_one({'chat_id': chat_id})['url']
+    def get_watermark_url(self, user_id):
+        return self.collection.find_one({"user_id": user_id})["url"]
