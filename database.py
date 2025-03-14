4dc5e75d982dc9a58ae85e69418a4d79ce45c0cc
blandk77 <alexags1983@gmail.com>
2025-03-14 15:41:52 +0530

    Update database.py

--- database.py
+++ database.py
@@ -8,7 +8,10 @@ class MongoDB:
         self.collection = self.db["watermark_urls"]
 
     def save_watermark_url(self, url, user_id):
-        self.collection.insert_one({"user_id": user_id, "url": url})
+        self.collection.update_one({"user_id": user_id}, {"$set": {"url": url}}, upsert=True)
 
     def get_watermark_url(self, user_id):
         return self.collection.find_one({"user_id": user_id})["url"]
+
+    def remove_watermark_url(self, user_id):
+        self.collection.delete_one({"user_id": user_id})
