cbda2ac191e2bd3fc82e478e2ef783e92b7fd879
blandk77 <alexags1983@gmail.com>
2025-03-15 20:11:48 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import logging
 from pyrogram import Client
 from pyrogram.types import Message
 import ffmpeg
+import subprocess
 
 logging.basicConfig(level=logging.INFO)
 
@@ -18,8 +19,8 @@ def add_overlay(client: Client, message: Message, overlay_image_id, user_id):
     # Overlay the image on the video using ffmpeg
     logging.info("Overlaying image on video...")
     output_video = os.path.join("/app", f"{user_id}.mp4")
-    ffmpeg.input(video).overlay(ffmpeg.input(overlay_image), eof_action='pass').output(output_video, vcodec='copy', acodec='copy', map_metadata=0).run()
-
+    command = f"ffmpeg -i {video} -i {overlay_image} -filter_complex overlay -map_metadata 0 -c:v copy -c:a copy {output_video}"
+    subprocess.run(command, shell=True)
     # Send the output video with the original caption
     logging.info("Sending output video...")
     caption = message.caption
