afec1bddbc062e5f0fef6c624c761dbb87520192
blandk77 <alexags1983@gmail.com>
2025-03-15 12:49:34 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 import os
-from moviepy import VideoFileClip, CompositeVideoClip, ImageClip
+from moviepy import VideoFileClip, ImageClip, CompositeVideoClip
 
-def add_overlay(video_file, overlay_image):
+def add_overlay(message, overlay_image):
+    video_file = message.download()
+    overlay_file = f"{overlay_image}.jpg"
+    client.download_media(overlay_image, file_name=overlay_file)
     video = VideoFileClip(video_file)
-    overlay = ImageClip(overlay_image).set_duration(video.duration)
+    overlay = ImageClip(overlay_file).set_duration(video.duration)
     final_video = CompositeVideoClip([video, overlay])
     output_file = os.path.join(config.UPLOAD_DIR, "output.mp4")
     final_video.write_videofile(output_file)
