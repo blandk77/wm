7ce3c80d311914d3c1b5905ce6825533fff6beb3
blandk77 <alexags1983@gmail.com>
2025-03-15 15:00:18 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,4 @@
 import pymongo
-from config import MONGO_COLLECTION_NAME, MONGO_DB_NAME, MONGO_URI
-
 
 class MongoDB:
     def __init__(self, mongo_uri, db_name, collection_name):
@@ -11,14 +9,11 @@ class MongoDB:
         self.db = self.client[self.db_name]
         self.collection = self.db[self.collection_name]
 
-    def add_overlay_image(self, overlay_image):
-        self.collection.insert_one({"overlay_image": overlay_image})
+    def add_overlay_image(self, user_id, overlay_image):
+        self.collection.update_one({"user_id": user_id}, {"$set": {"overlay_image": overlay_image}}, upsert=True)
 
-    def remove_overlay_image(self):
-        self.collection.delete_many({})
-
-    def get_overlay_image(self):
-        overlay_image = self.collection.find_one({"overlay_image": {"$exists": True}})
-        if overlay_image:
-            return overlay_image["overlay_image"]
+    def get_overlay_image(self, user_id):
+        user_data = self.collection.find_one({"user_id": user_id})
+        if user_data:
+            return user_data["overlay_image"]
         return None
