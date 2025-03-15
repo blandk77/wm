26d7b31c713a50670801881757767079ede41224
blandk77 <alexags1983@gmail.com>
2025-03-15 11:13:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ API_HASH = "90a9aa4c31afa3750da5fd686c410851"
 BOT_TOKEN = "7773651775:AAEH1TN8P5700Ni7fluT9A7uE0xUWuZ0slE"
 
 # Maximum allowed video size in bytes (e.g., 50 MB)
-MAX_VIDEO_SIZE = 50 × 1024 × 1024  # 50 MB
+MAX_VIDEO_SIZE = 50*1024*1024  # 50 MB
 
 # Temporary directory for storing video files
 TEMP_DIR = "./temp/"
