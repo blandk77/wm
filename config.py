2cb05f4ff9bc84f2b44da44e316ef1123d07cf66
blandk77 <alexags1983@gmail.com>
2025-03-15 11:33:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,10 @@ API_ID = "27394279"
 API_HASH = "90a9aa4c31afa3750da5fd686c410851"
 BOT_TOKEN = "7773651775:AAEH1TN8P5700Ni7fluT9A7uE0xUWuZ0slE"
 
-# Maximum allowed video size in bytes (e.g., 50 MB)
-MAX_VIDEO_SIZE = 50*1024*1024  # 50 MB
+# MongoDB connection details
+MONGO_URI = "mongodb+srv://telegramguy21:tnkIwvbNkJ5U3fZ7@botsuse.bpgag.mongodb.net/?retryWrites=true&w=majority&appName=Botsuse"
+DB_NAME = "telegram_bot_db"
+COLLECTION_NAME = "overlay_images"
 
 # Temporary directory for storing video files
 TEMP_DIR = "./temp/"
